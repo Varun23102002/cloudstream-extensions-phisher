2a1d9db86802cbb1934b7c6018cbe7187153309d
Varun23102002 <99336510+Varun23102002@users.noreply.github.com>
2025-06-26 11:27:21 +0530

    Update MPlayer.kt
    Tv shows view all episode fix

--- MPlayerProvider/src/main/kotlin/com/MPlayer/MPlayer.kt
+++ MPlayerProvider/src/main/kotlin/com/MPlayer/MPlayer.kt
@@ -22,10 +22,10 @@ class MPlayer : MainAPI() {
     override var mainUrl = "https://www.mxplayer.in"
     override var name = "M Player"
     override val hasMainPage = true
-    private var imageUrl="https://qqcdnpictest.mxplay.com/"
+    private var imageUrl = "https://qqcdnpictest.mxplay.com/"
     private var userID: String? = null
     private val webApi = "https://api.mxplayer.in/v1/web"
-    private val endpointurl="https://d3sgzbosmwirao.cloudfront.net/"
+    private val endpointurl = "https://d3sgzbosmwirao.cloudfront.net/"
     private val endParam
         get() = "&device-density=2&userid=$userID&platform=com.mxplay.desktop&content-languages=hi,en&kids-mode-enabled=false"
 
@@ -39,79 +39,74 @@ class MPlayer : MainAPI() {
             referer = "$mainUrl/"
         ).toString()
         val dramaRoot: MXPlayer = Gson().fromJson(dramaResponse, object : TypeToken<MXPlayer>() {}.type)
-        val dramashows = dramaRoot.items.map { item ->
-            item.toSearchResult()
-        }
+        val dramashows = dramaRoot.items.map { it.toSearchResult() }
 
         val crimeResponse = app.get(
             "$webApi/detail/browseItem?&pageNum=1&pageSize=20&isCustomized=true&genreFilterIds=b413dff55bdad743c577a8bea3b65044&type=2$endParam",
             referer = "$mainUrl/"
         ).toString()
         val crimeRoot: MXPlayer = Gson().fromJson(crimeResponse, object : TypeToken<MXPlayer>() {}.type)
-        val crime_shows = crimeRoot.items.map { item ->
-            item.toSearchResult()
-        }
+        val crime_shows = crimeRoot.items.map { it.toSearchResult() }
 
         val thrillerResponse = app.get(
             "$webApi/detail/browseItem?&pageNum=1&pageSize=20&isCustomized=true&genreFilterIds=2dd5daf25be5619543524f360c73c3d8&type=2$endParam",
             referer = "$mainUrl/"
         ).toString()
         val thrillerRoot: MXPlayer = Gson().fromJson(thrillerResponse, object : TypeToken<MXPlayer>() {}.type)
-        val thriller_shows = thrillerRoot.items.map { item ->
-            item.toSearchResult()
-        }
+        val thriller_shows = thrillerRoot.items.map { it.toSearchResult() }
 
         val hindimovieresponse = app.get(
             "$webApi/detail/browseItem?&pageNum=$page&pageSize=20&isCustomized=true&browseLangFilterIds=hi&type=1$endParam",
-            referer = "$mainUrl/").toString()
+            referer = "$mainUrl/"
+        ).toString()
         val movieRoot: MovieRoot = Gson().fromJson(hindimovieresponse, object : TypeToken<MovieRoot>() {}.type)
-        val hindi_Movies = movieRoot.items.map { item ->
-            item.toSearchResult()
-        }
+        val hindi_Movies = movieRoot.items.map { it.toSearchResult() }
 
         val telgumovieresponse = app.get(
             "$webApi/detail/browseItem?&pageNum=$page&pageSize=20&isCustomized=true&browseLangFilterIds=te&type=1$endParam",
-            referer = "$mainUrl/").toString()
+            referer = "$mainUrl/"
+        ).toString()
         val movieRootte: MovieRoot = Gson().fromJson(telgumovieresponse, object : TypeToken<MovieRoot>() {}.type)
-        val telgu_Movies = movieRootte.items.map { item ->
-            item.toSearchResult()
-        }
-        val Dramashows = HomePageList("Drama Shows", dramashows)
-        val HindiMovies = HomePageList("Hindi Movies", hindi_Movies)
-        val telguMovies = HomePageList("Telgu Movies", telgu_Movies)
-        val crimeshows = HomePageList("Crime Shows", crime_shows)
-        val thrillershows = HomePageList("Thriller Shows", thriller_shows)
+        val telgu_Movies = movieRootte.items.map { it.toSearchResult() }
 
-        return newHomePageResponse(listOf(crimeshows,Dramashows,thrillershows,HindiMovies,telguMovies))
+        return newHomePageResponse(
+            listOf(
+                HomePageList("Crime Shows", crime_shows),
+                HomePageList("Drama Shows", dramashows),
+                HomePageList("Thriller Shows", thriller_shows),
+                HomePageList("Hindi Movies", hindi_Movies),
+                HomePageList("Telgu Movies", telgu_Movies)
+            )
+        )
     }
 
-    //Movie classes
     private fun MovieItem.toSearchResult(): SearchResponse {
         val portraitLargeImageUrl = getPortraitLargeImageUrl(this)
-        val bigpic=getMBigPic(this)
-        return newMovieSearchResponse(title, LoadUrl(this.title, this.titleContentImageInfo,bigpic,this.type,this.stream,this.description,this.shareUrl,null).toJson()) {
+        val bigpic = getMBigPic(this)
+        return newMovieSearchResponse(
+            title,
+            LoadUrl(this.title, this.titleContentImageInfo, bigpic, this.type, this.stream, this.description, this.shareUrl, null).toJson()
+        ) {
             posterUrl = portraitLargeImageUrl
         }
     }
 
     private fun Item.toSearchResult(): SearchResponse {
         val portraitLargeImageUrl = getPortraitLargeImageUrl(this)
-        return newMovieSearchResponse(title, LoadUrl(this.title, this.titleContentImageInfo,null,this.type,null,this.description,this.shareUrl,null).toJson()) {
+        return newMovieSearchResponse(
+            title,
+            LoadUrl(this.title, this.titleContentImageInfo, null, this.type, null, this.description, this.shareUrl, null).toJson()
+        ) {
             posterUrl = portraitLargeImageUrl
         }
     }
 
     fun getPortraitLargeImageUrl(item: MovieItem): String? {
-        return item.imageInfo
-            .firstOrNull { it.type == "portrait_large" }
-            ?.url?.let { imageUrl + it }
+        return item.imageInfo.firstOrNull { it.type == "portrait_large" }?.url?.let { imageUrl + it }
     }
 
-
     fun getPortraitLargeImageUrl(item: Item): String? {
-        return item.imageInfo
-            .firstOrNull { it.type == "portrait_large" }
-            ?.url?.let { imageUrl + it }
+        return item.imageInfo.firstOrNull { it.type == "portrait_large" }?.url?.let { imageUrl + it }
     }
 
     fun getMovieBigPic(jsonString: String): String? {
@@ -122,9 +117,7 @@ class MPlayer : MainAPI() {
     }
 
     fun getMBigPic(item: MovieItem): String? {
-        return item.imageInfo
-            .firstOrNull { it.type == "bigpic" }
-            ?.url?.let { imageUrl + it }
+        return item.imageInfo.firstOrNull { it.type == "bigpic" }?.url?.let { imageUrl + it }
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
@@ -138,34 +131,38 @@ class MPlayer : MainAPI() {
         val result = mutableListOf<SearchResponse>()
         searchResult.sections.forEach { section ->
             section.items.forEach { item ->
+                val portraitLargeImageUrl = getBigPic(item)
                 if (item.type.contains("movie", ignoreCase = true)) {
-                    val portraitLargeImageUrl = getBigPic(item)
                     val streamUrl: String = item.stream?.hls?.high?.let { endpointurl + it }
                         ?: item.stream?.thirdParty?.hlsUrl
                         ?: item.stream?.thirdParty?.dashUrl
                         ?: ""
-                    result.add(newMovieSearchResponse(item.title, LoadUrl(item.title, item.titleContentImageInfo,null, item.type, null, item.description, item.shareUrl,streamUrl,portraitLargeImageUrl).toJson()) {
-                        posterUrl = portraitLargeImageUrl
-                    })
+                    result.add(
+                        newMovieSearchResponse(
+                            item.title,
+                            LoadUrl(item.title, item.titleContentImageInfo, null, item.type, null, item.description, item.shareUrl, streamUrl, portraitLargeImageUrl).toJson()
+                        ) {
+                            posterUrl = portraitLargeImageUrl
+                        }
+                    )
                 } else {
-                    val portraitLargeImageUrl = getBigPic(item)
-                    result.add(newMovieSearchResponse(item.title, LoadUrl(item.title, item.titleContentImageInfo,null, item.type, null, item.description, item.shareUrl,null,portraitLargeImageUrl).toJson()) {
-                        posterUrl = portraitLargeImageUrl
-                    })
+                    result.add(
+                        newMovieSearchResponse(
+                            item.title,
+                            LoadUrl(item.title, item.titleContentImageInfo, null, item.type, null, item.description, item.shareUrl, null, portraitLargeImageUrl).toJson()
+                        ) {
+                            posterUrl = portraitLargeImageUrl
+                        }
+                    )
                 }
             }
         }
-
         return result
     }
 
     fun getBigPic(item: Item): String? {
-        return item.imageInfo
-            .firstOrNull { it.type == "bigpic" }
-            ?.url?.let { imageUrl + it }
+        return item.imageInfo.firstOrNull { it.type == "bigpic" }?.url?.let { imageUrl + it }
     }
-
-
 
     override suspend fun load(url: String): LoadResponse? {
         val gson = Gson()
@@ -182,13 +179,18 @@ class MPlayer : MainAPI() {
         val title = video.title
         val poster = getMovieBigPic(url) ?: video.titleContentImageInfo ?: video.alternativeposter
         val type = if (video.tvType.contains("tvshow", true)) TvType.TvSeries else TvType.Movie
-        val href = video.stream?.hls?.high ?: video.stream?.thirdParty?.hlsUrl ?:video.stream?.hls?.base?: video.stream?.thirdParty?.hlsUrl ?: video.stream?.thirdParty?.dashUrl ?:video.alternativestream
+        val href = video.stream?.hls?.high
+            ?: video.stream?.thirdParty?.hlsUrl
+            ?: video.stream?.hls?.base
+            ?: video.stream?.thirdParty?.dashUrl
+            ?: video.alternativestream
+
         return if (type == TvType.TvSeries) {
             val epposter = getMovieBigPic(url)
             val seasonData = getSeasonData("$mainUrl${video.shareUrl}")
             val episodes = mutableListOf<Episode>()
             seasonData.forEach { (season, id) ->
-                val apiUrl = "$webApi/detail/tab/tvshowepisodes?type=season&id=$id"
+                val apiUrl = "$webApi/detail/tab/tvshowepisodes?type=season&id=$id&limit=1000"
                 val jsonResponse = app.get(apiUrl).toString()
 
                 val episodesParser = try {
@@ -203,15 +205,15 @@ class MPlayer : MainAPI() {
                     val name = it.title ?: "Unknown Title"
                     val image = imageUrl + it.imageInfo.map { img -> img.url }.firstOrNull()
                     val episode = index + 1
-                    episodes += newEpisode(href1)
-                    {
-                        this.name=name
-                        this.season=season+1
-                        this.episode=episode
-                        this.posterUrl=image
+                    episodes += newEpisode(href1) {
+                        this.name = name
+                        this.season = season + 1
+                        this.episode = episode
+                        this.posterUrl = image
                     }
                 }
             }
+
             newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
                 posterUrl = epposter ?: video.alternativeposter
                 backgroundPosterUrl = epposter
@@ -232,58 +234,30 @@ class MPlayer : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        if (data.startsWith("video")) {
-            val href = endpointurl + data
-            callback.invoke(
-                newExtractorLink(
-                    this.name,
-                    name,
-                    url = href,
-                    INFER_TYPE
-                ) {
-                    this.referer = "$mainUrl/"
-                    this.quality = Qualities.Unknown.value
-                }
-            )
-        } else {
-            callback.invoke(
-                newExtractorLink(
-                    this.name,
-                    name,
-                    url = data,
-                    INFER_TYPE
-                ) {
-                    this.referer = "$mainUrl/"
-                    this.quality = Qualities.Unknown.value
-                }
-            )
-        }
+        val href = if (data.startsWith("video")) endpointurl + data else data
+        callback.invoke(
+            newExtractorLink(this.name, name, href, INFER_TYPE) {
+                this.referer = "$mainUrl/"
+                this.quality = Qualities.Unknown.value
+            }
+        )
         return true
-    }}
+    }
+}
 
 private suspend fun getSeasonData(url: String): List<Pair<Int, String>> {
     val document = app.get(url).document
     return document.select("div.hs__items-container > div").mapNotNull { element ->
         val tab = element.attr("data-tab").toIntOrNull()
         val id = element.attr("data-id")
-        if (tab != null && id.isNotBlank()) {
-            tab to id
-        } else {
-            null
-        }
+        if (tab != null && id.isNotBlank()) tab to id else null
     }
 }
 
-
 private fun Headers.getCookies(cookieKey: String = "set-cookie"): Map<String, String> {
-    // Get a list of cookie strings
-    // set-cookie: name=value; -----> name=value
-    val cookieList =
-        this.filter { it.first.equals(cookieKey, ignoreCase = true) }.map {
-            it.second.split(";").firstOrNull()
-        }.filterNotNull()
+    val cookieList = this.filter { it.first.equals(cookieKey, ignoreCase = true) }
+        .mapNotNull { it.second.split(";").firstOrNull() }
 
-    // [name=value, name2=value2] -----> mapOf(name to value, name2 to value2)
     return cookieList.associate {
         val split = it.split("=", limit = 2)
         (split.getOrNull(0)?.trim() ?: "") to (split.getOrNull(1)?.trim() ?: "")
@@ -293,7 +267,7 @@ private fun Headers.getCookies(cookieKey: String = "set-cookie"): Map<String, St
 data class LoadUrl(
     val title: String,
     val titleContentImageInfo: List<Any>? = emptyList(),
-    val bigpic:String? = null,
+    val bigpic: String? = null,
     val tvType: String,
     val stream: MovieStream? = null,
     val description: String,
